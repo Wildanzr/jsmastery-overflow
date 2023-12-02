7bea4e2c9d46d2f29fcfde234496bc8b75be35b4
wildanzr <graita.sukma@gmail.com>
2023-12-02 15:34:26 +0700

    feat: basic auth with clerk

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import { UserButton } from "@clerk/nextjs";
 
-const HomePage = () => {
-  return <div>HomePage</div>;
-};
-
-export default HomePage;
+export default function HomePage() {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+}
