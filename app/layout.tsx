aacbdd30540b6c36c68b332ec47a2629e27b67b2
wildanzr <graita.sukma@gmail.com>
2023-12-07 21:28:28 +0700

    feat: metadata and assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,42 @@
 import { ClerkProvider } from "@clerk/nextjs";
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from 'next/font/google'
+import type { Metadata } from "next";
+
+import './globals.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+})
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-space-grotesk',
+})
+
+export const metadata: Metadata = {
+  title: 'DevFlow',
+  description: 'DevFlow is a platform for developers to share their knowledge and experience with others.',
+  icons: {
+    icon: '/assets/images/site-logo.svg',
+  }
+}
 
 export default function RootLayout({ children }: ChildrenProps) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary-500'
+        }
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>{children}</body>
       </html>
     </ClerkProvider>
   );
