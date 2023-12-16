a8da0205cca7d18abd3a8d6dc64ca71d3e9c3899
wildanzr <graita.sukma@gmail.com>
2023-12-16 22:16:45 +0700

    feat: base navbar and theme context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,47 @@
 import { ClerkProvider } from "@clerk/nextjs";
 // eslint-disable-next-line camelcase
-import { Inter, Space_Grotesk } from 'next/font/google'
+import { Inter, Space_Grotesk } from "next/font/google";
 import type { Metadata } from "next";
 
-import './globals.css'
+import "./globals.css";
+import { ThemeProvider } from "@/contexts/ThemeProvider";
 
 const inter = Inter({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-inter',
-})
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
 
 const spaceGrotesk = Space_Grotesk({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700'],
-  variable: '--font-space-grotesk',
-})
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-space-grotesk",
+});
 
 export const metadata: Metadata = {
-  title: 'DevFlow',
-  description: 'DevFlow is a platform for developers to share their knowledge and experience with others.',
+  title: "DevFlow",
+  description:
+    "DevFlow is a platform for developers to share their knowledge and experience with others.",
   icons: {
-    icon: '/assets/images/site-logo.svg',
-  }
-}
+    icon: "/assets/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({ children }: ChildrenProps) {
   return (
-    <ClerkProvider
-      appearance={{
-        elements: {
-          formButtonPrimary: 'primary-gradient',
-          footerActionLink: 'primary-text-gradient hover:text-primary-500'
-        }
-      }}
-    >
-      <html lang="en">
-        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>{children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500",
+            },
+          }}
+        >
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
---
+++ contexts/ThemeProvider.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+"use client"
+
+import React, { createContext, useContext, useState, useEffect } from "react"
+
+interface ThemeContextType {
+  mode: string;
+  setMode: (mode: string) => void;
+  handleThemeChange: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
+
+export function ThemeProvider({ children}: { children: React.ReactNode}) {
+  const [mode, setMode] = useState<string>('');
+
+  const handleThemeChange = () => {
+    if (mode === 'dark') {
+      setMode('light')
+      document.documentElement.classList.add('light')
+    } else {
+      setMode('dark')
+      document.documentElement.classList.add('dark')
+    }
+  }
+
+  const _exports = {
+    mode,
+    setMode,
+    handleThemeChange
+  }
+
+  useEffect(() => {
+    handleThemeChange()
+  }, [])
+
+  return (
+    <ThemeContext.Provider value={_exports}>
+      {children}
+    </ThemeContext.Provider>
+  )
+} 
+
+export function useTheme() {
+  const context = useContext(ThemeContext)
+
+  if (context === undefined) {
+    throw new Error("useTheme must be used within a ThemeProvider")
+  }
+
+  return context
+}
