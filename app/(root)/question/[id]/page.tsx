e93d31bc355128318621f37885f43ee5e180f282
wildanzr <graita.sukma@gmail.com>
2024-03-15 21:58:35 +0700

    feat: base question detail page

---
+++ app/(root)/question/[id]/page.tsx
@@ -0,0 +1,75 @@
+import { getQuestionById } from '@/lib/actions/question.action'
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+import Metric from '@/components/shared/Metric'
+import { formatAndDivideNumber, getTimestamp } from '@/lib/utils'
+import ParseHTML from '@/components/shared/ParseHTML'
+import RenderTag from '@/components/shared/RenderTag'
+import Answer from '@/components/forms/Answer'
+
+const QuestionDetailPage = async ({ params, searchParams}: any) => {
+  const { question} = await getQuestionById({ questionId: params.id })
+  return (
+    <>
+      <div className="flex-start w-full flex-col">
+        <div className='flex w-full flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2'>
+          <Link className='flex items-center justify-start gap-1 ' href={`/profile/${question.author.clerkId}`}>
+            <Image src={question.author.picture} alt="user" width={22} height={22} />
+            <p className='paragraph-semibold text-dark300_light700'>
+              {question.author.name}
+            </p>
+          </Link>
+
+          <div className="flex justify-end">
+            VOTING
+          </div>
+        </div>
+
+        <h2 className='h2-semibold text-dark200_light900 mt-3.5 w-full text-left'>
+          {question.title}
+        </h2>
+      </div>
+
+      <div className='mb-8 mt-5 flex flex-wrap gap-4'>
+      <Metric
+          imgUrl="/assets/icons/clock.svg"
+          alt="clock icon"
+          value={`Asked ${getTimestamp(question.createdAt)}`}
+          title=""
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/message.svg"
+          alt="answers"
+          value={formatAndDivideNumber(question.answers.length)}
+          title="Answers"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/eye.svg"
+          alt="Eye"
+          value={question.views}
+          title="Views"
+          textStyles="small-medium text-dark400_light800"
+        />
+      </div>
+
+      <ParseHTML data={question.content} />
+
+      <div className='mt-8 flex flex-wrap gap-2'>
+        {question.tags.map((tag: any, index: number) => (
+          <RenderTag key={index} 
+          _id={tag._id}
+          name={tag.name}
+          showCount={false} 
+          />
+        ))}
+      </div>
+
+      <Answer />
+    </>
+  )
+}
+
+export default QuestionDetailPage
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -3,7 +3,7 @@
 import Question from "@/database/question.model";
 import { connectToDatabase } from "../mongoose";
 import Tag from "@/database/tag.model";
-import { CreateQuestionParams, GetQuestionsParams } from "./shared.types";
+import { CreateQuestionParams, GetQuestionByIdParams, GetQuestionsParams } from "./shared.types";
 import User from "@/database/user.model";
 import { revalidatePath } from "next/cache";
 
@@ -22,6 +22,23 @@ export const getQuestions = async (params: GetQuestionsParams) => {
     throw error;
   }
 };
+
+export const getQuestionById = async (params: GetQuestionByIdParams) => {
+  try {
+    connectToDatabase();
+
+    const { questionId } = params;
+    const question = await Question.findById(questionId)
+      .populate({ path: "tags", model: Tag, select: "_id name" })
+      .populate({ path: "author", model: User, select: "_id clerkId name picture" });
+
+    return { question };
+
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
 
 export const createQuestion = async (params: CreateQuestionParams) => {
   try {
